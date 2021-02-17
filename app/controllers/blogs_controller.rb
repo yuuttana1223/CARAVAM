cb1de2a59d7e138934bef35c324d2de8b628888f
EC2 Default User <ec2-user@ip-172-31-84-126.ec2.internal>
2021-02-17 16:01:42 +0000

    add update and delete

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,9 +23,15 @@ class BlogsController < ApplicationController
   end
 
   def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog.id)
   end
 
   def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
