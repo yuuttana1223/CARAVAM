66c60bc71bbba532a5f631da210497df39d8e232
EC2 Default User <ec2-user@ip-172-31-84-126.ec2.internal>
2021-02-18 02:10:51 +0000

    debug

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,8 @@ class BlogsController < ApplicationController
 
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -14,6 +16,7 @@ class BlogsController < ApplicationController
 
   def create
     blog = Blog.new(blog_params)
+    binding.pry
     blog.save
     redirect_to blogs_path(blog.id)
   end
